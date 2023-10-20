9932012ece27dcebe0d2bc7041a6a907bdcc99c7
Abhishek <abhishek.raman018@gmai.com>
2023-10-21 01:12:46 +0530

    blogsite

---
+++ src/components/Eventbutton.tsx
@@ -0,0 +1,124 @@
+'use client'
+import React, { useState, useRef, useEffect } from "react";
+
+export default function StatusButton() {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const closeDropdown = () => {
+    setDropdownVisible(false);
+  };
+
+  // Listen for clicks outside the button and dropdown to close the dropdown
+  const handleDocumentClick = (e: MouseEvent) => {
+    if (buttonRef.current && !buttonRef.current.contains(e.target as Node)) {
+      closeDropdown();
+    }
+  };
+
+  // Add a click event listener when the component mounts
+  useEffect(() => {
+    document.addEventListener("click", handleDocumentClick);
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, []);
+
+  return (
+    <div style={{ position: "relative" }}>
+      <button
+        ref={buttonRef}
+        id="dropdownRadioBgHoverButton"
+        onClick={toggleDropdown}
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover-bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Events{" "}
+        <svg
+          className="w-2.5 h-2.5 ml-2.5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 6"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m1 1 4 4 4-4"
+          />
+        </svg>
+      </button>
+
+      {dropdownVisible && (
+        <div
+          id="dropdownRadioBgHover"
+          className="z-10 w-64 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+          style={{ position: "absolute", top: "100%", right: 0 }}
+        >
+          <ul
+            className="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
+            aria-labelledby="dropdownRadioBgHoverButton"
+          >
+            <li>
+              <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                <input
+                  id="default-radio-4"
+                  type="radio"
+                  value=""
+                  name="default-radio"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  htmlFor="default-radio-4"
+                  className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+                >
+                 All Events
+                </label>
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                <input
+                  checked
+                  id="default-radio-5"
+                  type="radio"
+                  value=""
+                  name="default-radio"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  htmlFor="default-radio-5"
+                  className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+                >
+                  Hackathons
+                </label>
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                <input
+                  id="default-radio-6"
+                  type="radio"
+                  value=""
+                  name="default-radio"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  htmlFor="default-radio-6"
+                  className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+                >
+                 Talk-Shows
+                </label>
+              </div>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/components/SingleEvent.tsx
@@ -0,0 +1,8 @@
+export default function SingleEvent(){
+  
+    return(
+        <div className="flex justify-center  flex-rows mt-24 max-w-full mr-5 ml-5 p-8 rounded t-lg bg-white">
+          <span className="text-black">Hello</span>
+        </div>
+    )
+};
