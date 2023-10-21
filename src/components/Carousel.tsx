291ef7d67d085aa1723bb4c4ea3607dad730d40a
Abhishek <abhishek.raman018@gmai.com>
2023-10-21 16:13:15 +0530

    carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -21,11 +21,15 @@ export default function Carousel() {
   const [activeIndex, setActiveIndex] = useState<number>(2); // Set the active index to 2 or any other starting index
 
   const prevImage = () => {
-    setActiveIndex((prevIndex) => (prevIndex === 0 ? carouselImg.length - 1 : prevIndex - 1));
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? carouselImg.length - 1 : prevIndex - 1
+    );
   };
 
   const nextImage = () => {
-    setActiveIndex((prevIndex) => (prevIndex === carouselImg.length - 1 ? 0 : prevIndex + 1));
+    setActiveIndex((prevIndex) =>
+      prevIndex === carouselImg.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   const handleImageLoad = (index: number) => {
@@ -37,7 +41,11 @@ export default function Carousel() {
   };
 
   return (
-    <div id="animation-carousel" className="relative w-full" data-carousel="static">
+    <div
+      id="animation-carousel"
+      className="relative w-full"
+      data-carousel="static"
+    >
       <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
         {carouselImg.map((item, index) => (
           <div
@@ -66,7 +74,24 @@ export default function Carousel() {
         data-carousel-prev
         onClick={prevImage}
       >
-        {/* Previous button code here */}
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <svg
+            className="w-4 h-4 text-white dark:text-gray-800"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 1 1 5l4 4"
+            />
+          </svg>
+          <span className="sr-only">Previous</span>
+        </span>
       </button>
       <button
         type="button"
@@ -74,7 +99,24 @@ export default function Carousel() {
         data-carousel-next
         onClick={nextImage}
       >
-        {/* Next button code here */}
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <svg
+            className="w-4 h-4 text-white dark:text-gray-800"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 9 4-4-4-4"
+            />
+          </svg>
+          <span className="sr-only">Next</span>
+        </span>
       </button>
     </div>
   );
